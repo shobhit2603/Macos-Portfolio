0d28e75b4325d6f56299dabb0bd5d935eb7cfc4c
shobhit2603 <shrivastavashobhit706@gmail.com>
2025-11-26 16:13:04 +0530

    Created Image and Text-File Window

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import gsap from 'gsap';
 import { Draggable } from 'gsap/Draggable';
 
 import { Navbar, Welcome, Dock } from '@components';
-import { Terminal, Safari, Resume, Finder } from '@windows';
+import { Terminal, Safari, Resume, Finder, Text, Image } from '@windows';
 
 gsap.registerPlugin(Draggable);
 
@@ -19,6 +19,8 @@ const App = () => {
       <Safari />
       <Resume />
       <Finder />
+      <Text />
+      <Image />
     </main>
   )
 }
--- src/windows/Finder.jsx
+++ src/windows/Finder.jsx
@@ -12,6 +12,7 @@ const Finder = () => {
 
     const openItem = (item) => {
         if (item.fileType === 'pdf') return openWindow('resume');
+        if (item.fileType === 'txt') return openWindow('txtfile', item);
         if (item.kind === 'folder') return setActiveLocation(item);
         if (['fig', 'url'].includes(item.fileType) && item.href) return window.open(item.href, '_blank');
 
